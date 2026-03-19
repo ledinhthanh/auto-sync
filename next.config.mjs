18de3682de73afc75f058c60cd5003032e9f67c8
Lê Đình Thành <mrthanh.ledinh@outlook.com>
2026-03-19 11:00:03 +0700

    feat: Add `cpu-features` to `serverExternalPackages` in Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['ssh2', 'node-ssh'],
+  serverExternalPackages: ['ssh2', 'node-ssh', 'cpu-features'],
 };
 
 export default nextConfig;
